c5da66ac9559a46b098579aca6492bf95e844a90
han xu <hanxu27@gmail.com>
2019-03-01 01:35:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { blah: "Moonlight Sonata" }
 
 function updatePlaylist(playlist, name, title){
-  playlist = Object.assign({}, playlist, { name: title})
+  playlist[name] = title
   return playlist
 }
