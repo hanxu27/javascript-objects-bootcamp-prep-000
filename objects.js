f26c60226c7b8192266a45e6eebf4da9faa0e616
han xu <hanxu27@gmail.com>
2019-03-01 01:31:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Beethoven: "Moonlight Sonata" }
+var playlist = { blah: "Moonlight Sonata" }
 
 function updatePlaylist(playlist, name, title){
   playlist = Object.assign({}, playlist, { name: title})
